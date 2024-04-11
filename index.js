97e68d4fcd416504a107c68a44c3e5bcf48db48d
Ramz <84850359+Ramz001@users.noreply.github.com>
2024-04-11 18:43:34 +0500

    final version planets I/O done

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const parse = require("csv-parse")
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
+const results = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.33 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(parse({ columns: true, comment: "#" }))
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
+  })
+  .on("end", () => {
+    console.log(results.map(planet => planet["kepler_name"]))
+    console.log(`${results.length} habitable planets found`);
+  })
+  .on("error", (err) => console.log(err));
